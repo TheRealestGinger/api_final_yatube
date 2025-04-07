598e4a0f9512d4907ed01dc5b1b116df8eb77efc
Nikitka <nik.nikitosa@mail.ru>
2025-04-07 08:28:42 +0300

    7 commit

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -5,27 +5,33 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField('Заголовок', max_length=200)
+    slug = models.SlugField('Идентификатор', unique=True)
+    description = models.TextField('Описание')
+
+    class Meta:
+        verbose_name = 'группа'
+        verbose_name_plural = 'Группы'
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField('Текст')
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        'Автор', User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        'Картинка', upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(
-        Group, on_delete=models.SET_NULL,
+        'Группа', Group, on_delete=models.SET_NULL,
         related_name='posts', blank=True, null=True
     )
 
     class Meta:
+        verbose_name = 'пост'
+        verbose_name_plural = 'Посты'
         ordering = ('-pub_date',)
 
     def __str__(self):
@@ -34,19 +40,29 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        'Автор', User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        'Пост', Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField('Текст')
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
 
 
 class Follow(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='follower')
+        'Подписчик', User, on_delete=models.CASCADE, related_name='follower')
     following = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='following')
+        'Пользователь', User,
+        on_delete=models.CASCADE, related_name='following'
+    )
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'Подписки'
 
     def __str__(self):
         return f'{self.user} follows {self.following}'
